34fe24f84287c197cac251b2e71a6533102ac06e
tjdcks8601 <tjdcks8601@naver.com>
2026-08-13 22:37:04 +0900

    import selenium

--- Portfolio_for_exit_edwards.py
+++ Portfolio_for_exit_edwards.py
@@ -1,1 +1,11 @@
-print("Portfolio for exit edwards")
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+driver = webdriver.Chrome()
+driver.get('https://www.google.com')
+
+time.sleep(3)
+
+
+
