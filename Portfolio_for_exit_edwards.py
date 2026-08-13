0917952c62da2b38a46e6e3b0e59ce968b00414b
tjdcks8601 <tjdcks8601@naver.com>
2026-08-13 23:14:23 +0900

    crawling in google

--- Portfolio_for_exit_edwards.py
+++ Portfolio_for_exit_edwards.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 import time
 
 driver = webdriver.Chrome()
@@ -7,5 +8,9 @@ driver.get('https://www.google.com')
 
 time.sleep(3)
 
+search_box = driver.find_element(By.NAME, "q")
 
+search_box.send_keys("파이썬")
+search_box.send_keys(Keys.RETURN)
+time.sleep(3)
 
