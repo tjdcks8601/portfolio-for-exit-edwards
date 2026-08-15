a8c2c476a3ae70588b6b53ebbf53b03fb336cd87
tjdcks8601 <tjdcks8601@naver.com>
2026-08-15 21:56:01 +0900

    api_modified_260815

--- API.py
+++ API.py
@@ -1,1 +1,13 @@
 import requests
+
+def SearchMovies(query, year) :
+    params = {
+        "api_key" : "bd8a0564139e17aeaec0a040e09be1be",
+        "language" : "ko",
+        "page" : "1",
+        "query" : query,
+        "include_adult" : "true",
+        "year" : year
+    }
+
+    url = "https://api.themoviedb.org/3/search/movie"
