c888299bd35fa09c80e662ab01ce8d9733aac174
tjdcks8601 <tjdcks8601@naver.com>
2026-08-15 16:42:53 +0900

    add input

--- selenium_web_crawling.py
+++ selenium_web_crawling.py
@@ -1,3 +1,16 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+import time
 
+keyword = input("수집할 이미지 : ")
+
+url = "https://search.naver.com/search.naver?ssc=tab.image.all&where=image&query={}".format(keyword)
+driver = webdriver.Chrome()
+driver.implicitly_wait(5)
+driver.maximize_window()
+
+
+driver.get(url)
+
+time.sleep(3)
+driver.quit()
