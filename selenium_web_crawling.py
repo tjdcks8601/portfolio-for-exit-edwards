34194571b36e9b2a947bb3f302cef06cafd5282b
tjdcks8601 <tjdcks8601@naver.com>
2026-08-15 17:01:35 +0900

    selenium_webcrawling2

--- selenium_web_crawling.py
+++ selenium_web_crawling.py
@@ -1,5 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
 
 keyword = input("수집할 이미지 : ")
@@ -8,7 +11,6 @@ url = "https://search.naver.com/search.naver?ssc=tab.image.all&where=image&query
 driver = webdriver.Chrome()
 driver.implicitly_wait(5)
 driver.maximize_window()
-
 
 driver.get(url)
 
