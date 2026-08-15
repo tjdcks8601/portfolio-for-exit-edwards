78aecb3a89864be1fbcdbe24bbbf6d7e8cd239aa
tjdcks8601 <tjdcks8601@naver.com>
2026-08-15 19:11:58 +0900

    howsibalhimdeulda

--- selenium_web_crawling.py
+++ selenium_web_crawling.py
@@ -1,31 +1,17 @@
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support import expected_conditions as EC # 실무기준
+from selenium.webdriver.common.by import By
 import time
 
-keyword = input("수집할 이미지 : ")
-
-url = "https://search.naver.com/search.naver?ssc=tab.image.all&where=image&query={}".format(keyword)
 driver = webdriver.Chrome()
-driver.implicitly_wait(5)
-driver.maximize_window()
 
-driver.get(url)
+driver.get("http://www.google.com")
 
-try:
-    WebDriverWait(driver, 10).until(
-        EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'img.__fe_image_tab_content_thumbnail_image'))
-    )
-except Exception as e:
-    print(f"요소 로드 실패: {e}")
+print(driver.title)
 
-for _ in range(5):
-    driver.execute_script("window.scrollBy(0, window.innerHeight);")
-    time.sleep(0.5)
+stp1 = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="gb"]/div[1]/div[1]/a')))
+stp1.click()
 
-imgs = driver.find_elements(By.CSS_SELECTOR, 'img.__fe_image_tab_content_thumbnail_image')
-print(f"찾은 이미지: {len(imgs)}개")
-
-time.sleep(3)
+time.sleep(5)
 driver.quit()
